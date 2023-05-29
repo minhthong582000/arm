ae0e9dd282e4d0b7f3c8fe7e767854f39c2bb436
minhthong582000 <55283557+minhthong582000@users.noreply.github.com>
2023-05-29 10:40:27 +0700

    added tf backend
    Signed-off-by: minhthong582000 <55283557+minhthong582000@users.noreply.github.com>

--- deploy/terraform/backends.tf
+++ deploy/terraform/backends.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket                      = "mys3bucket"
     key                         = "demo/terraform/terraform.tfstate"
-    endpoint                    = "http://localhost:9000"
+    endpoint                    = "http://minio:9000"
     region                      = "us-west-2"
     skip_credentials_validation = true # Skip AWS related checks and validations
     skip_metadata_api_check     = true
